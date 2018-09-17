1a722fd38c8c66ed783eea2c14e858f6f6c66edd
bryan <158237404@qq.com>
2018-09-17 14:59:19 +0800

    First commit.

---
+++ app/src/main/java/com/mingweather/android/util/HttpUtil.java
@@ -0,0 +1,9 @@
+package com.mingweather.android.util;
+
+/**
+ * Created by Administrator on 2018/9/17.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback)
+}
